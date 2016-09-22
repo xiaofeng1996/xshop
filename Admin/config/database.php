d957938d735d44514b0f22f33e1ca1505c7df291
llzif <1297192749@qq.com>
2016-09-22 21:31:39 +0800

    商品sku Signed-off-by: llzif <1297192749@qq.com>

--- Admin/config/database.php
+++ Admin/config/database.php
@@ -75,12 +75,12 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'hostname' => '123.56.145.227',
+	'username' => 'zhangsan',
+	'password' => 'root',
+	'database' => 'xshop',
 	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
+	'dbprefix' => 'x_',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
 	'cache_on' => FALSE,
